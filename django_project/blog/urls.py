1d69872a157eefd0e27b8586e495215f485658ee
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-03-31 00:10:54 +1030

    django_part02_09_add about page

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views # . means current repository 
 
+# better always put '/' after the address
 urlpatterns = [
     #'' means home page, 
     # it means if people add nothing after the website address,
     # then return the home page. And the home page is on views.home
     path('', views.home, name='blog-home'),  
+    path('about/', views.about, name='blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -5,3 +5,5 @@ def home(request): # handle the traffic from home page.
     return HttpResponse('<h1>Blog Home</h1>')
 
 
+def about(request): # about page
+    return HttpResponse('<h1>Blog About</h1>')
