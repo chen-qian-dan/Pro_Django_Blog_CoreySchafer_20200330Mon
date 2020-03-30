97d86c6cc116bd14281f4bf2420d27566838e530
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-03-31 02:52:10 +1030

    django_part03_16_add posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Qian', 
+        'title': 'ML',
+        'content': 'Machine Learning',
+        'date_posted': 'March 31, 2020',
+    },
+    {
+        'author': 'Chen', 
+        'title': 'CV',
+        'content': 'Computer Vision',
+        'date_posted': 'March 31, 2020',
+    }
+]
+
 def home(request): # handle the traffic from home page.
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context) # then home.html can access posts by the key 'posts'
 
 
 def about(request): # about page
