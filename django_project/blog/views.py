34b5da530160198e8293dcee11eb54f8a20a1f83
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-03-31 02:27:13 +1030

    django_part03_15_edit about.html and link it to views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request): # handle the traffic from home page.
     return render(request, 'blog/home.html')
 
 
 def about(request): # about page
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
