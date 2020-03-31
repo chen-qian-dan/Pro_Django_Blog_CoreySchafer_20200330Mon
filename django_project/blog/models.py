94c63989afa7217128cecbd86b12d5cf30167614
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-03-31 23:38:47 +1030

    django_part05_26_create database

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    # attribute is field in database
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    # on_delete=models.CASCADE means if the user is deleted, 
+    # we want to delete all the posts from that user as well. 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
