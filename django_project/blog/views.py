bca313b3b7c0196a48d5089ee6774f9706052a3f
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-02 02:59:14 +1030

    Comment the dummy posts out, we don't need it anymore.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Qian', 
-        'title': 'Blog 1: ML',
-        'content': 'Machine Learning',
-        'date_posted': 'March 31, 2020',
-    },
-    {
-        'author': 'Chen', 
-        'title': 'Blog 2: CV',
-        'content': 'Computer Vision',
-        'date_posted': 'March 31, 2020',
-    }
-]
+# posts = [
+#     {
+#         'author': 'Qian', 
+#         'title': 'Blog 1: ML',
+#         'content': 'Machine Learning',
+#         'date_posted': 'March 31, 2020',
+#     },
+#     {
+#         'author': 'Chen', 
+#         'title': 'Blog 2: CV',
+#         'content': 'Computer Vision',
+#         'date_posted': 'March 31, 2020',
+#     }
+# ]
 
 def home(request): # handle the traffic from home page.
     context = {
