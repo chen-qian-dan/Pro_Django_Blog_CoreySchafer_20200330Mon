81b905c6fc15b297c3200a43163b68fea20bd134
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-02 15:14:21 +1030

    minor

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    form = UserCreationForm
+    form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
