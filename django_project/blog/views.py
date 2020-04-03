17e65be6da194d48872c46a3f6a7af70c840b3d9
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-03 22:43:23 +1030

    django_part10_61_delete view

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
 # posts = [
@@ -59,6 +65,16 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request): # about page
