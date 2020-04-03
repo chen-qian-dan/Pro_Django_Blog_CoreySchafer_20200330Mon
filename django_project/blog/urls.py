b18e75c95812110d6494a2f52d62e6709226491f
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-03 21:13:59 +1030

    django_part10_52_change views.home to PostListView

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import PostListView
 from . import views # . means current repository 
 
 # better always put '/' after the address
@@ -6,6 +7,6 @@ urlpatterns = [
     #'' means home page, 
     # it means if people add nothing after the website address,
     # then return the home page. And the home page is on views.home
-    path('', views.home, name='blog-home'),  
+    path('', PostListView.as_view(), name='blog-home'),  
     path('about/', views.about, name='blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # posts = [
@@ -21,6 +22,13 @@ def home(request): # handle the traffic from home page.
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context) # then home.html can access posts by the key 'posts'
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+
 
 
 def about(request): # about page
